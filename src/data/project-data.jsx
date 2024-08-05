fe2b4d81883f74fb8b2a64dd502aadcbcabf5d36
zaki <zaki@Zakis-MacBook-Air.local>
2024-08-06 06:48:29 +0700

    updating project-data

--- src/data/project-data.jsx
+++ src/data/project-data.jsx
@@ -1,32 +1,44 @@
-import project1 from '../assets/Project1.jpg';
-import project2 from '../assets/Keeper Project.jpg';
-import project3 from '../assets/Project3.jpg';
+import project1 from "../assets/Project1.jpg";
+import project2 from "../assets/Keeper Project.jpg";
+import project4 from "../assets/Project4.png";
 
 const projectData = [
-    {
-        title:"Portofolio Website Project",
-        year:"2024",
-        tags:"HTML, CSS, JavaScript, PostgreSQL",
-        description:"First ever Web Development Project. The website where i'm gonna put all my Software Development progress. This website will continue to be updated.",
-        imgSrc: project1,
-        more: false,
-    },
-    {
-        title:"Keeper App Clone",
-        year:"2024",
-        tags:"React.js, JavaScript, HTML, CSS, PostgreSQL",
-        description:"This is a Keeper App clone. You can post your notes in this app. Caution when using because the app is yet to connect to database!",
-        imgSrc: project2,
-        more: false,
-    },
-    {
-        title:"Portofolio Website Project Rework (Work In Progress)",
-        year:"2024",
-        tags:"React.js, JavaScript, HTML, CSS, Express, Node.js",
-        description:"The website where i'm gonna put all my Software Development progress. This website will continue to be updated. This website is a rework of the previous Portofolio Website using React",
-        imgSrc: project1,
-        more: false,
-    }
-]
+  {
+    title: "Portofolio Website Project",
+    year: "2024",
+    tags: "HTML, CSS, JavaScript, PostgreSQL",
+    description:
+      "First ever Web Development Project. The website where i'm gonna put all my Software Development progress. This website will continue to be updated.",
+    imgSrc: project1,
+    more: false,
+  },
+  {
+    title: "Keeper App Clone",
+    year: "2024",
+    tags: "React.js, JavaScript, HTML, CSS, PostgreSQL",
+    description:
+      "This is a Keeper App clone. You can post your notes in this app. Caution when using because the app is yet to connect to database!",
+    imgSrc: project2,
+    more: false,
+  },
+  {
+    title: "Portofolio Website Project Rework (Work In Progress)",
+    year: "2024",
+    tags: "React.js, JavaScript, HTML, CSS, Express, Node.js",
+    description:
+      "The website where i'm gonna put all my Software Development progress. This website will continue to be updated. This website is a rework of the previous Portofolio Website using React",
+    imgSrc: project1,
+    more: false,
+  },
+  {
+    title: "Fullstack API Blog Project",
+    year: "2024",
+    tags: "RESTful API, Backend, CSS, Express, Node.js",
+    description:
+      "Creating a blog API Project complete with frontend and backend server and also the API server. Not yet connected to database",
+    imgSrc: project4,
+    more: false,
+  },
+];
 
 export default projectData;
